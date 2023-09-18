68c3201fe4c42fc001ca26d60a084ffb778574e7
nicvagn <nicolasvaagen@gmail.com>
2023-09-18 10:33:42 -0600

    first

---
+++ app.js
@@ -0,0 +1,41 @@
+//lost to time what this does
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
+
+
+//configure and display chess board.js
+var chessPosition = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R';
+var correctPosition = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R';
+var config = {
+  draggable: true,
+  dropOffBoard: 'snapback', // this is the default
+  position: chessPosition
+}
+var puzzle = ChessBoard('pBoard', config);
+
+//reset chessboard button
+$('#resetBoard').on('click', function () {
+  puzzle.position(chessPosition);
+})
+
+//reset chess puzzle on button click
+function resetPuzzle(){
+  puzzle.position(chessPosition);
+}
+
+//check chess puzzle
+function checkPuzzle(){
+  if(puzzle.fen() == correctPosition){
+    window.alert("Correct! Great job!");
+  }
+  else{
+    window.alert("Try again");
+  }
+}
+
+//get random puzzle
